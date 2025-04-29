9d8e7364d0e493edabd64c7a193d557f1f970516
PratamaAngga <anggashillnoir@gmail.com>
2025-04-29 16:16:42 +0700

    selesai percobaan2

--- Jobsheet9/StackTugasMahasiswa20.java
+++ Jobsheet9/StackTugasMahasiswa20.java
@@ -75,4 +75,18 @@ public class StackTugasMahasiswa20 {
         }
         System.out.println("");
     }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi stack = new StackKonversi();
+        while (nilai != 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
